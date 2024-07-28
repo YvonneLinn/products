dee4cde819f47b00a269fbe3ff8ac86c1b642707
Yvonne Lin <yvonn50921@gmail.com>
2024-07-28 15:29:41 +0800

    add for loop for printing products

--- products.py
+++ products.py
@@ -4,10 +4,9 @@ while True:
 	if name == 'q': #quit
 		break
 	price = input('請輸入商品價格:')
-	p = [] #小火車
-	p.append(name) #p = [name, price]
-	p.append(price)
-	products.append(p) #products.append([name, price])
+	products.append([name, price])
 print(products) #二維清單
 
-products[0][0] #第0格中的第0格
+for p in products: #小火車
+	print(p[0], '的價格是', p[1])
+
