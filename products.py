87eeb0f8b3e769a8b3b5caf03ac524a7061f87e8
Yvonne Lin <yvonn50921@gmail.com>
2024-08-04 15:37:36 +0800

    add check file exist

--- products.py
+++ products.py
@@ -1,12 +1,18 @@
+import os #operating system
+
 #讀取檔案
 products = [] #大火車
-with open('products.csv', 'r', encoding='utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue #和break一樣只能寫在迴圈內，continue是跳到下一迴的意思
-		name, price = line.strip().split(',') #由左到右：先把換行去掉，再把","當切割的標準(切割的結果是清單)
-		products.append([name, price])
-print(products)
+if os.path.isfile('products.csv'): #檢查檔案在不在
+	print('yeah!找到檔案了!')
+	with open('products.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue #和break一樣只能寫在迴圈內，continue是跳到下一迴的意思
+			name, price = line.strip().split(',') #由左到右：先把換行去掉，再把","當切割的標準(切割的結果是清單)
+			products.append([name, price])
+	print(products)
+else:
+	print('找不到檔案......')
 
 #讓使用者輸入
 while True:
@@ -25,7 +31,7 @@ for p in products: #小火車
 #字串可以加法'abc' + '123' = 'abc123'
 #字串可以乘法'abc' * 3 = 'abcabcabc'
 
-#寫入檔案
+#寫入檔案：逗號會讓EXCEL為不同格
 with open ('products.csv', 'w', encoding='utf-8') as f: #一般有OPEN就有CLOSE，但WITH會自動CLOSE，utf-8編碼
 	f.write('商品,價格\n')
 	for p in products:
